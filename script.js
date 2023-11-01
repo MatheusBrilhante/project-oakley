cf424edecf3fff900634cbb25b792bf82a409bc7
Matheus Brilhante <matheus_brilhante1@hotmail.com>
2023-11-01 20:59:42 -0300

    att 59

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ sr.reveal('.container3-5', { duration: 1500 });
 sr.reveal('.container4', { duration: 1500 });
 sr.reveal('.container5', { duration: 1500 });
 sr.reveal('.container6', { duration: 1500 });
+
+//onst imgs = document.getElementById('sec2-1');
+//const img = document.querySelectorAll('#img img');
+
+//let idx = 0;
+
+//function carrossel(){
+ //   idx++;
+ //   if(idx > img.length - 1){
+//        idx = 0;
+ //   }
+
+//   imgs.style.transform = `translatex(${-idx * 400}px)`;
+//}
+
+//setInterval(carrossel,1800);//
+

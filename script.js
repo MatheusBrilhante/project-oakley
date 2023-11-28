ee99d70264ecf2d1cf61ce21c62047fb43dec323
Matheus Brilhante <matheus_brilhante1@hotmail.com>
2023-11-27 21:42:33 -0300

    att72

--- script.js
+++ script.js
@@ -25,39 +25,85 @@ sr.reveal('.container4', { duration: 1000 });
 sr.reveal('.container5', { duration: 1000 });
 sr.reveal('.container6', { duration: 1000 });
 // Obtenha o header
-let header = document.getElementById("myheader");
-// Obtenha a posição inicial do header
-let ini = header.offsetTop;
- // Adicione um listener de rolagem à janela
-window.onscroll = function(){
-    // Verifique se a posição da rolagem é maior que a posição inicial do header
-    if(window.pageYOffset > ini){
-        // Se sim, adicione a classe fixed ao header
-        header.classList.add("fixed");
+
+let prevscroll = window.pageYOffset;
+
+window.addEventListener("scroll", function(){
+    let crscrollpos = window.pageYOffset;
+    
+    if(prevscroll > crscrollpos){
+         // Rola para cima: mostra o header
+        document.getElementById("myheader").style.top = "10px";
     } else{
-        // Se não, remova a classe fixed do header
-        header.classList.remove("fixed")
+         // Rola para baixo: esconde o header
+        document.getElementById("myheader").style.top ="-80px";
     }
-}
 
-
+    prevscroll = crscrollpos;
+});
 //<!-- Adicione um identificador à imagem para referência em JavaScript -->
   //<img id="myImage" src="small-image.jpg" alt="Imagem Pequena" onmouseover="changeImage()" onmouseout="resetImage()"></img>
 
 
+// ! 1 Oculos //
+
+
 // Função chamada quando o mouse passa sobre a imagem
-function changeImage(){
+function changeImage1(){
      // Altere o atributo src para a imagem maior
-    document.getElementById("myImage") .src="img.oc/oc5 - Copia";
-    document.getElementById("myImage2") .src="img.oc/oc6.png";
-    document.getElementById("myImage3") .src="img.oc/oc14t.png";
+    document.getElementById("myImage").src="img.oc/oc5 - Copia.png";
+    
+ //ADICIONANDO UMA POSICAO FIXA NA TELA    
+    document.getElementById("bt1").style.marginTop = '50px' 
+
 }
 // Função chamada quando o mouse sai da imagem
-function resetImage(){
+function resetImage1(){
     // Restaure o atributo src para a imagem original
     document.getElementById("myImage") .src="img.oc/oc12t.png";
+
+    //ADICIONANDO UMA POSICAO FIXA NA TELA    
+    document.getElementById("bt1").style.marginTop = '-5px' 
+
 }
 
+
+// ! 2 Oculos //
+function changeImage2(){
+    // Altere o atributo src para a imagem maior
+    document.getElementById("myImage") .src="img.oc/oc6.png";
+
+    //ADICIONANDO UMA POSICAO FIXA NA TELA    
+    document.getElementById("bt1").style.marginTop = '50px'
+
+}
+// Função chamada quando o mouse sai da imagem
+function resetImage2(){
+   // Restaure o atributo src para a imagem original
+    document.getElementById("myImage") .src="img.oc/oc12t.png";
+
+    //ADICIONANDO UMA POSICAO FIXA NA TELA    
+    document.getElementById("bt1").style.marginTop = '-5px'
+}
+
+
+// ! 3 Oculos //
+function changeImage3(){
+    // Altere o atributo src para a imagem maior
+    document.getElementById("myImage") .src="img.oc/oc8.png";
+
+    //ADICIONANDO UMA POSICAO FIXA NA TELA    
+    document.getElementById("bt1").style.marginTop = '50px'
+
+}
+// Função chamada quando o mouse sai da imagem
+function resetImage3(){
+   // Restaure o atributo src para a imagem original
+    document.getElementById("myImage") .src="img.oc/oc12t.png";
+
+    //ADICIONANDO UMA POSICAO FIXA NA TELA    
+    document.getElementById("bt1").style.marginTop = '-5px'
+}
 
 
 //const imgs = document.getElementById('sec2-1');
